7baec1a68bd90110d04353e4b4f5818dc8bcfc0c
Randal Kamradt <randysr@kamradtfamily.net>
2018-03-23 16:09:34 -0700

    turn on and off led

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,9 +1,20 @@
+var gpio = require("pi-gpio");
+
 module.exports = function(app, db) {
   app.get('/led/:id', (req, res) => {
-    res.send('Blinking led ' + req.params.id)
+    gpio.open(40, "output", function(err) {		// Open pin 16 for output
+    	gpio.write(40, 1, function() {			// Set pin 16 high (1)
+    		gpio.close(40);						// Close pin 16
+    	});
+    });
+    res.send('turning on led ' + req.params.id)
   });
-  app.post('/led', (req, res) => {
-    // You'll blink your led here.
-    res.send('Hello');
+  app.post('/led/:id', (req, res) => {
+    gpio.open(40, "output", function(err) {		// Open pin 16 for output
+    	gpio.write(40, 0, function() {			// Set pin 16 high (1)
+    		gpio.close(40);						// Close pin 16
+    	});
+    });
+    res.send('turning off led ' + req.params.id)
   });
 };
