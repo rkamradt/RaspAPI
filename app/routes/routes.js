268ec79074ab26c750dc038427266f4472ce46ff
Randal Kamradt <randysr@kamradtfamily.net>
2018-03-24 07:15:36 -0700

    added logging

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,15 +2,18 @@ var gpio = require("rpi-gpio");
 
 module.exports = function(app, db) {
   app.get('/led/:id', (req, res) => {
-    gpio.setup(21, gpio.DIR_OUT, function(err) {		// Open pin 16 for output
-    	gpio.write(21, true, function() {			// Set pin 16 high (1)
+    console.log("in get");
+    gpio.setup(21, gpio.DIR_OUT, function(err) {		// Open pin 21 for output
+      console.log("after setup err:" + err);
+    	gpio.write(21, true, function(err) {			// Set pin 21 high (1)
+        console.log("after write err:" + err);
     	});
     });
     res.send('turning on led ' + req.params.id)
   });
   app.post('/led/:id', (req, res) => {
-    gpio.setup(21, "output", function(err) {		// Open pin 16 for output
-    	gpio.write(21, false, function() {			// Set pin 16 high (1)
+    gpio.setup(21, gpio.DIR_OUT, function(err) {		// Open pin 21 for output
+    	gpio.write(21, false, function() {			// Set pin 21 high (1)
     	});
     });
     res.send('turning off led ' + req.params.id)
