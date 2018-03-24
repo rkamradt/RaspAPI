8ddb21ef64cc6bb3f3bc883eeadc636d5bf283a3
Randal Kamradt <randysr@kamradtfamily.net>
2018-03-24 07:28:50 -0700

    try pin number instead of gpio number

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,17 +3,17 @@ var gpio = require("rpi-gpio");
 module.exports = function(app, db) {
   app.get('/led/:id', (req, res) => {
     console.log("in get");
-    gpio.setup(21, gpio.DIR_OUT, function(err) {		// Open pin 21 for output
+    gpio.setup(40, gpio.DIR_OUT, function(err) {		// Open pin 40 for output
       console.log("after setup err:" + err);
-    	gpio.write(21, true, function(err) {			// Set pin 21 high (1)
+    	gpio.write(40, true, function(err) {			// Set pin 40 high (1)
         console.log("after write err:" + err);
     	});
     });
     res.send('turning on led ' + req.params.id)
   });
   app.post('/led/:id', (req, res) => {
-    gpio.setup(21, gpio.DIR_OUT, function(err) {		// Open pin 21 for output
-    	gpio.write(21, false, function() {			// Set pin 21 high (1)
+    gpio.setup(40, gpio.DIR_OUT, function(err) {		// Open pin 40 for output
+    	gpio.write(40, false, function() {			// Set pin 40 low (0)
     	});
     });
     res.send('turning off led ' + req.params.id)
