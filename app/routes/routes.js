7a963b22f4c954832a5bf91ced9263d61b7e939f
Randal Kamradt <randysr@kamradtfamily.net>
2018-03-23 17:34:29 -0700

    use rpio-gpio lib

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,18 +1,16 @@
-var gpio = require("pi-gpio");
+var gpio = require("rpi-gpio");
 
 module.exports = function(app, db) {
   app.get('/led/:id', (req, res) => {
-    gpio.open(40, "output", function(err) {		// Open pin 16 for output
-    	gpio.write(40, 1, function() {			// Set pin 16 high (1)
-    		gpio.close(40);						// Close pin 16
+    gpio.setup(21, gpio.DIR_OUT, function(err) {		// Open pin 16 for output
+    	gpio.write(21, true, function() {			// Set pin 16 high (1)
     	});
     });
     res.send('turning on led ' + req.params.id)
   });
   app.post('/led/:id', (req, res) => {
-    gpio.open(40, "output", function(err) {		// Open pin 16 for output
-    	gpio.write(40, 0, function() {			// Set pin 16 high (1)
-    		gpio.close(40);						// Close pin 16
+    gpio.setup(21, "output", function(err) {		// Open pin 16 for output
+    	gpio.write(21, false, function() {			// Set pin 16 high (1)
     	});
     });
     res.send('turning off led ' + req.params.id)
