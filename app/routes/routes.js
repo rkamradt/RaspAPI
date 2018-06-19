0f595b6cd9d141a484002b9251773ca212005a94
Randal Kamradt (CW) <rkamradt@rkamradt006043.starbucks.net>
2018-06-19 09:29:53 -0700

    added wiring-pi package to use SPI

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,25 @@
 var gpio = require("rpi-gpio");
+var wpi = require('wiring-pi');
+
+wpi.setup('wpi');
 
 module.exports = function(app, db) {
+  app.post('/number/:number', (req, res) => {
+    const fd = wpi.wiringPiSPISetup(0, 500000)
+    if(fd === -1) {
+      throw Error("setup returned error")
+    }
+    const data = Buffer.alloc(5)
+    for (let i = 0; i < 5; i++) {
+      data[i] = i;
+    }
+    const res = wpi.wiringPiSPIDataRW(0, data)
+    console.log('response from DataRW = ' + res)
+    for (const value of data.values()) {
+      console.log(value);
+    }
+    wpi.wiringPiSPIClose(fd)
+  }
   app.get('/led/:id', (req, res) => {
     const id = req.params.id;
     console.log("in get turning on pin " + id);
